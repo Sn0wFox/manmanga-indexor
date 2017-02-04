b85b00ae9eec58b96782573c0a2b784f3d87d1bd
Sn0wFox <ruben.pericas@laposte.net>
2017-02-04 16:48:57 +0100

    Added lib files

---
+++ src/lib.spec.ts
@@ -0,0 +1,32 @@
+import { Client } from 'indexden-client';
+import * as Lib from './lib';
+
+describe('lib', () => {
+  let client: Client = new Client(process.env.INDEXDEN_ENDPOINT);
+
+  describe('.ensureIndex()', () => {
+    let originalTimeout: number = 0;
+
+    beforeAll(() => {
+      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 12000;
+    });
+
+    it('should ensure that the index exists', (done: any) => {
+      let indexName = 'test';
+      Lib
+        .ensureIndex(client, indexName)
+        .then(() => {
+          return client.removeIndex(indexName);
+        })
+        .then(() => {
+          done();
+        })
+    });
+
+    afterAll(() => {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+  });
+
+});
---
+++ src/lib.ts
@@ -0,0 +1,26 @@
+import * as Bluebird        from 'bluebird';
+import { RequestError }     from 'request-promise/errors';
+import { Client, Indexes }  from 'indexden-client';
+
+/**
+ * Ensures that the requested index exists in the given client.
+ * @param client The indexden client in which verify if the index exists.
+ * @param indexName The index's name to verify.
+ * @returns {Bluebird<void>}
+ */
+export function ensureIndex(client: Client, indexName: string): Bluebird<void> {
+  return client
+    .getIndexesMetadata(indexName)
+    .then((data: Indexes.Metadata) => {
+      // The index already exists, nothing else to do!
+      return;
+    })
+    .catch((err: RequestError) => {
+      if(err.error.statusCode == 404) {
+        // The index doesn't exists yet! Let's create it
+        return client.createOrUpdateIndex(indexName);
+      }
+      // There was a much bigger problem, we'd better reject all of it!
+      return Bluebird.reject(err);
+    });
+}
