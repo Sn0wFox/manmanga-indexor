37896b04cbf91ad8fcfa1e329e631568206451cd
Sn0wFox <ruben.pericas@laposte.net>
2017-02-24 11:04:05 +0100

    Refactored index

--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,23 @@
-import * as Indexden  from 'indexden-client';
 import * as Bluebird  from 'bluebird';
-import * as Lib       from './lib';
-import * as Dbpedia   from './dbpedia';
+import { Indexor }    from './indexor';
 
-/**
- * Main constants and default values.
- */
-const client: Indexden.Client               = new Indexden.Client(process.env.INDEXDEN_ENDPOINT);
-const default_max_doc_per_indexing: number  = 42;
-const default_start_indexing: number        = 0;
-const default_max_failures: number          = 3;
+const indexor: Indexor = new Indexor('manmanga');
 
 /**
  * Indexes every manga and anime.
- * @param from The offset of dbpedia doc to index.
  * @param flat The number of docs to index per loop.
  * @param maxFails The max number of consecutive failure before aborting.
  * @returns {Bluebird<any>}
  */
 export function totalIndexing(
-  from: number       = default_start_indexing,
-  flat: number       = default_max_doc_per_indexing,
-  maxFails: number   = default_max_failures
+  flat?: number,
+  maxFails?: number
 ): Bluebird<any> {
 
-  let nManga: number    = 0;
-  let nAnime: number    = 0;
-  let failures: number  = 0;
-  from = Math.floor(from/flat);
-
-  return Bluebird
-    .all([
-      Dbpedia
-        .countResources('dbo:Manga')
-        .then((n: number) => {
-          nManga = n;
-        }),
-      Dbpedia
-        .countResources('dbo:Anime')
-        .then((n: number) => {
-          nAnime = n;
-        }),
-      Bluebird.resolve(Lib.log('INFO: INIT - starting indexing job'))
-    ])
-    .then(() => {
-      // Indexing loop
-      Lib.log('INFO - GREAT: starting indexing ' + nManga + ' manga from ' + from + '.');
-      let i: number = from -1;
-      let type: string = 'dbo:Manga';
-      let num: number = nManga;
-      let done: boolean = false;
-      return Lib.promiseLoop(
-        (): boolean => {
-          i++;
-          if(i * flat < num + flat) {
-            return true;
-          }
-          if(!done) {
-            // All manga have been indexed
-            // Let's go for anime
-            Lib.log('INFO - GREAT: all manga indexed. Starting indexing ' + nAnime + ' anime.', 'info');
-            i = 0;
-            type = 'dbo:Anime';
-            num = nAnime;
-            done = true;
-            return true;
-          }
-          console.log("end");
-          Lib.log('INFO - GREAT: all anime indexed. DONE.', 'info');
-          return false;
-        },
-        (): Bluebird<any> => {
-          return Bluebird
-            .delay(1000)
-            .then(() => {
-              return Lib.indexDocs(client, 'manmanga', flat, i * flat, type);
-            })
-            .then((res: any[] | undefined) => {
-              failures = 0;
-              return Lib.log("INFO: SUCCESS - successfully indexed " + (res ? res.length : '???') + " files from " + i*flat + ".", 'info', res);
-            })
-            .catch((err: Error) => {
-              if(++failures === maxFails) {
-                // Too many consecutive failures
-                // We'd better abort the whole process
-                Lib.log('ERROR: FATAL - Too many consecutive failures. Aborting.', 'error', err);
-                return Bluebird.reject(err);
-              }
-              return Lib.log(
-                'ERROR: Problem indexing ' + flat + ' docs from ' + i*flat + '.', 'error');
-            });
-        }
-      );
+  return indexor
+    .manga(0, flat, maxFails)
+    .then((res: any) => {
+      return indexor.anime(0, flat, maxFails);
     });
 }
 
---
+++ src/indexor.ts
@@ -0,0 +1,117 @@
+import { Client }     from 'indexden-client';
+import * as Bluebird  from 'bluebird';
+import * as Lib       from './lib';
+import * as Dbpedia   from './dbpedia';
+
+export class Indexor {
+  // Default values
+  static readonly DEFAULT_MAX_DOC_PER_INDEXING: number  = 42;
+  static readonly DEFAULT_START_INDEXING: number        = 0;
+  static readonly DEFAULT_MAX_FAILURES: number          = 3;
+
+  /**
+   * The Indexden client to use.
+   */
+  private client: Client;
+
+  /**
+   * The index name to use.
+   */
+  private indexName: string;
+
+  /**
+   * Initialize the Indexor.
+   * @param indexName The index name to use with the client.
+   * @param client The client to use. Optional. A new one will be created by default.
+   */
+  public constructor(indexName: string, client?: Client) {
+    this.indexName = indexName;
+    this.client = client || new Client(process.env.INDEXDEN_ENDPOINT);
+  }
+
+  /**
+   * Index all indexable dbo:Manga.
+   * @param from
+   * @param flat
+   * @param maxFails
+   * @returns {Bluebird<any>}
+   */
+  public manga(
+    from?: number,
+    flat?: number,
+    maxFails?: number
+  ): Bluebird<any> {
+    return this.resourceIndexor('dbo:Manga', from, flat, maxFails);
+  }
+
+  /**
+   * Index all indexable dbo:Anime.
+   * @param from
+   * @param flat
+   * @param maxFails
+   * @returns {Bluebird<any>}
+   */
+  public anime(
+    from?: number,
+    flat?: number,
+    maxFails?: number
+  ): Bluebird<any> {
+    return this.resourceIndexor('dbo:Anime', from, flat, maxFails);
+  }
+  
+  protected resourceIndexor(
+    resource: string,
+    from: number       = Indexor.DEFAULT_START_INDEXING,
+    flat: number       = Indexor.DEFAULT_MAX_DOC_PER_INDEXING,
+    maxFails: number   = Indexor.DEFAULT_MAX_FAILURES
+  ): Bluebird<any> {
+
+    let nManga: number    = 0;
+    let failures: number  = 0;
+    from = Math.floor(from/flat);
+
+    return Dbpedia
+      .countResources(resource)
+      .then((n: number) => {
+        nManga = n;
+      })
+      .then(() => {
+        // Indexing loop
+        Lib.log('INFO - GREAT: starting indexing ' + nManga + ' ' + resource + ' from ' + from + '.');
+        let i: number = from -1;
+        let num: number = nManga;
+        return Lib.promiseLoop(
+          (): boolean => {
+            i++;
+            if(i * flat < num + flat) {
+              return true;
+            }
+            console.log("end");
+            Lib.log('INFO - GREAT: all manga indexed. DONE.', 'info');
+            return false;
+          },
+          (): Bluebird<any> => {
+            return Bluebird
+              .delay(1000)
+              .then(() => {
+                return Lib.indexDocs(this.client, this.indexName, flat, i * flat, resource);
+              })
+              .then((res: any[] | undefined) => {
+                failures = 0;
+                return Lib.log("INFO: SUCCESS - successfully indexed " + (res ? res.length : '???') + " files from " + i*flat + ".", 'info', res);
+              })
+              .catch((err: Error) => {
+                if(++failures === maxFails) {
+                  // Too many consecutive failures
+                  // We'd better abort the whole process
+                  Lib.log('ERROR: FATAL - Too many consecutive failures. Aborting.', 'error', err);
+                  return Bluebird.reject(err);
+                }
+                return Lib.log(
+                  'ERROR: Problem indexing ' + flat + ' docs from ' + i*flat + '.', 'error');
+              });
+          }
+        );
+      });
+  }
+}
